70df8e28126aa3e726c660365c0156178f58fb5f
bspencer <bspencer@conductor.com>
2016-12-09 16:55:57 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Haskell scripts and code snippets
+
+[Install the Haskell Platform](https://www.haskell.org/platform/#osx-homebrewcask)
---
+++ scripts/hello.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin stack
+--stack runghc --resolver lts-6.24 --install-ghc
+
+main = do 
+    putStrLn "Hello world!"
+    putStrLn "Hey!"
